7ace372a9c20c0e2e831fb8e307ff958f3b9bf50
kidkmon <kidmendesn@gmail.com>
2025-07-15 20:25:58 -0400

    Add player movement and camera follow

---
+++ Assets/Scripts/Core/Singleton.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+    public static T Instance => _instance ??= FindFirstObjectByType<T>();
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed = 5f;
+    [SerializeField] private float _rotationSpeed = 1f;
+
+    private CharacterController _controller;
+
+    private Animator _animator;
+    private int _animIDMoveInput;
+
+    private Vector2 _moveInput;
+    private Vector3 _moveDirection;
+
+    void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _controller = GetComponent<CharacterController>();
+    }
+
+    void Start()
+    {
+        AssignAnimationIDs();
+    }
+
+    void Update()
+    {
+        InputMagnitude();
+    }
+
+    void AssignAnimationIDs()
+    {
+        _animIDMoveInput = Animator.StringToHash("MoveInput");
+    }
+
+    void InputMagnitude()
+    {
+        float inputMagnitude = _moveInput.sqrMagnitude;
+
+        if (inputMagnitude > 0.1f)
+        {
+            _animator.SetFloat(_animIDMoveInput, inputMagnitude, .1f, Time.deltaTime);
+            PlayerMoveAndRotate();
+        }
+        else
+        {
+            _animator.SetFloat(_animIDMoveInput, inputMagnitude, .1f, Time.deltaTime);
+        }
+    }
+
+    void PlayerMoveAndRotate()
+    {
+        _moveDirection = new Vector3(_moveInput.x, 0, _moveInput.y);
+        _moveDirection.Normalize();
+
+        if (_moveDirection != Vector3.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(_moveDirection);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime);
+        }
+
+        Vector3 move = _moveDirection * _moveSpeed * Time.deltaTime;
+        _controller.Move(move);
+    }
+
+
+    #region PlayerInput 
+
+    public void OnMove(InputValue value)
+    {
+        _moveInput = value.Get<Vector2>();
+    }
+
+    #endregion
+}
